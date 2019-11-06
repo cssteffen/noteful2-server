96f31b0201523d1c8da5bbe8d3565fbc136e3619
Cara Steffen <cara@treadstonedesign.com>
2019-11-06 12:34:25 -0700

    express boilerplate with sql scripts for note and folder tables

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,11 +1,11 @@
 const FoldersService = {
   getAllFolders(knex) {
-    return knex.select("*").from("noteful_folders");
+    return knex.select("*").from("folders");
   },
   insertFolder(knex, newFolder) {
     return knex
       .insert(newFolder)
-      .into("noteful_folders")
+      .into("folders")
       .returning("*")
       .then(rows => {
         return rows[0];
@@ -13,18 +13,18 @@ const FoldersService = {
   },
   getById(knex, id) {
     return knex
-      .from("noteful_folders")
+      .from("folders")
       .select("*")
       .where("id", id)
       .first();
   },
   deleteFolder(knex, id) {
-    return knex("noteful_folders")
+    return knex("folders")
       .where({ id })
       .delete();
   },
   updateFolder(knex, id, newFolderFields) {
-    return knex("noteful_folders")
+    return knex("folders")
       .where({ id })
       .update(newFolderFields);
   }
--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -5,7 +5,7 @@ const NotesService = {
   insertNote(knex, newNote) {
     return knex
       .insert(newNote)
-      .into("noteful_notes")
+      .into("notes")
       .returning("*")
       .then(rows => {
         return rows[0];
@@ -13,18 +13,18 @@ const NotesService = {
   },
   getById(knex, id) {
     return knex
-      .from("noteful_notes")
+      .from("notes")
       .select("*")
       .where("id", id)
       .first();
   },
   deleteNote(knex, id) {
-    return knex("noteful_notes")
+    return knex("notes")
       .where({ id })
       .delete();
   },
   updateNote(knex, id, newNoteFields) {
-    return knex("noteful_notes")
+    return knex("notes")
       .where({ id })
       .update(newNoteFields);
   }
